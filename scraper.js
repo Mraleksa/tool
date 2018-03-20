cc92c5579f40e3d65a84982806012b35e895d518
Mraleksa <a.gribanovskiy@gmail.com>
2018-03-20 13:16:24 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -9,7 +9,7 @@ var db = new sqlite3.Database("data.sqlite");
 
 //db.each("SELECT dateModified FROM data ORDER BY dateModified DESC LIMIT 1", function(err, timeStart) {
 
-var start =  "2018-03-14T14:35:02.826703+02:00" 
+var start =  "2018-03-16T12:44:08.343993+02:00" 
 //var end  = formatTime(new Date());
 //var end  = "2017-01-03"
 var p=0; var p2=0;
