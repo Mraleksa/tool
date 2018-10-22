7e6b40d42fca89599d13817028752eff76d900df
Mraleksa <a.gribanovskiy@gmail.com>
2018-10-22 15:08:37 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -21,7 +21,7 @@ db.close();
 
 //db.each("SELECT dateModified FROM data ORDER BY dateModified DESC LIMIT 1", function(err, timeStart) {
 
-var start =  "2018-09-17T16:41:25.262477+03:00" 
+var start =  "2018-09-21T11:03:05.966639+03:00" 
 //var end  = formatTime(new Date());
 //var end  = "2017-01-03"
 var p=0; var p2=0;
